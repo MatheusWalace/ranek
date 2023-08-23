125c3f16e8de9f22def48ca6376e5b37bf53a294
MatheusDesenvolvedor <matheuswalace56@gmail.com>
2023-08-23 18:31:42 -0300

    feat: produtos

---
+++ src/components/ProdutosLista.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="produtos-container">
+    <div v-for="produto in produtos" :key="produto.id">
+      <img v-if="produto.fotos" :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo">
+      <p>{{produto.descricao}}</p>
+      <h2 class="titulo">{{produto.nome}}</h2>
+      <p class="preco">{{produto.preco}}</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      produtos: null
+    }
+  },
+  methods: {
+    getProdutos() {
+      fetch("http://localhost:3000/produto").then(response => response.json()).then(response => {
+        this.produtos = response
+      })
+    }
+  },
+  created() {
+    this.getProdutos()
+  }
+}
+</script>
+
+<style>
+
+</style>
